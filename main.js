29e509894c1e6cc2d52401eb5350e8a752b4f6d0
NPCgit <119781990+NPCgit@users.noreply.github.com>
2024-03-04 18:50:09 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function preload()
+{
+
+}
+
+function setup()
+{
+    canvas = createCanvas(640, 480);
+    fill(13, 14, 246)
+    circle(40, 120, 60)
+    fill(14, 238, 24)
+    rect(140, 200, 56, 43, 10)
+    fill(60, 60, 60)
+    ellipse(60, 100, 40)
+    fill(255, 200, 12)
+    circle(200, 45, 78)
+    
+}
+function draw()
+{
+    image(video, 0, 0, 640, 480);
+    tint(tint_color);
+}
+
